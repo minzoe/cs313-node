5285cd85926dbab65b792cbaca203c9b1a337f2a
minzoe <min16009@byui.edu>
2018-06-21 21:25:49 -0600

    'fixing'

--- index.js
+++ index.js
@@ -1,30 +1,82 @@
-const cool = require('cool-ascii-faces')
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-const {
-    Pool
-} = require('pg');
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
+var express = require('express');
+var app = express();
+var url = require('url');
+var path = require('path');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(path.join(__dirname + 'public')));
+
+app.set('/views', path.join(__dirname + 'views'));
+app.set('view engine', 'ejs');
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/form.html'));
+})
+app.get('/rate', function(req, res) {
+    handleForm(req, res);
 });
 
-express()
-    .use(express.static(path.join(__dirname, 'public')))
-    .set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'ejs')
-    .get('/', (req, res) => res.render('pages/index'))
-    .get('/cool', (req, res) => res.send(cool()))
-    .get('/db', async (req, res) => {
-        try {
-            const client = await pool.connect()
-            const result = await client.query('SELECT * FROM test_table');
-            res.render('pages/db', result);
-            client.release();
-        } catch (err) {
-            console.error(err);
-            res.send("Error " + err);
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
+
+function handleForm(req, res) {
+    var requestUrl = url.parse(req.url, true);
+
+    console.log("Query parameters: " + JSON.stringify(requestUrl.query));
+
+    var weight = requestUrl.query.weight;
+    var type = requestUrl.query.type;
+
+    computeRate(res, weight, type);
+}
+
+// lstamp Letters(Stamped)
+// lmeter Letters(Metered)
+// lgenv Large Envelopes
+// fcpsr First-Class Packaged Serive—Retail
+
+function computeRate(res, weight, type) {
+    var cost;
+    if (type == 'lstamp') {
+        if (weight < 3.5) {
+            cost = (0.50 + ((weight - 1) * 0.21)).toFixed(2);
+        } else {
+            cost = 1.13;
         }
-    })
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    } else if (type == 'lmeter') {
+        if (weight < 3.5) {
+            cost = (0.47 + ((weight - 1) * 0.21)).toFixed(2);
+        } else {
+            cost = 1.10;
+        }
+    } else if (type == 'lgenv') {
+        if (weight < 13) {
+            cost = (1.00 + ((weight - 1) * 0.21)).toFixed(2);
+        } else {
+            cost = 3.52;
+        }
+    } else if (type == 'fcpsr') {
+        if (weight < 4) {
+            cost = 3.50;
+        } else if (weight < 8) {
+            cost = 3.75;
+        } else if (weight < 9) {
+            cost = 4.10;
+        } else if (weight < 10) {
+            cost = 4.45;
+        } else if (weight < 11) {
+            cost = 4.80;
+        } else if (weight < 12) {
+            cost = 5.15;
+        } else if (weight < 13) {
+            cost = 5.50;
+        }
+    } else {
+        console.error("There was an error" + res.error);
+    }
+
+    var params = {weight: weight, type: type, result: cost};
+
+    res.render('\getRate', params);
+}
